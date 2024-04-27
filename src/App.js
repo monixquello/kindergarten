d75110fd694284ca7f342544881834993cebd749
monixquello <monicahmirembe540@gmail.com>
2024-04-27 08:55:04 +0300

    new changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function App() {
   return (
     <Routes>
-      <Route path='/home' element={<Home/>} />
+      <Route path='/' element={<Home/>} />
       <Route path='/about' element={<About/>}/>
       <Route path='/contact' element={<Contact/>}/>
     </Routes>
